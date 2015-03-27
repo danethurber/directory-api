a42bbc9bcc28d1a5a0507cb4ba0562025ecb0d98
Dane Thurber <dane.thurber@gmail.com>
2015-03-27 10:48:57 -0600

    hello world

---
+++ server/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.use(function(req, res, next) {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  next();
+});
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+module.exports = app;
